c7e6cc17d6ff7c0fb5baa015a8a06b852e3f6f31
Erno Hentonen <erno.hentonen@opiframe.com>
2024-09-16 14:21:37 +0300

    shopping app 1

--- react/50_shopping_app/01_stage/backend/routes/shoppingroute.js
+++ react/50_shopping_app/01_stage/backend/routes/shoppingroute.js
@@ -35,4 +35,33 @@ router.post("/shopping",function(req,res) {
 	})
 })
 
+router.delete("/shopping/:id",function(req,res) {
+	itemModel.deleteOne({"_id":req.params.id}).then(function(){
+		return res.status(200).json({"Message":"Success"});
+	}).catch(function(err) {
+		console.log("Failed deleting items, Reason",err)
+		return res.status(500).json({"Message":"Internal server error"});
+	})
+})
+
+router.put("/shopping/:id",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad request"});
+	}
+	if(!req.body.type) {
+		return res.status(400).json({"Message":"Bad request"});
+	}
+	let item = {
+		type:req.body.type,
+		count:req.body.count,
+		price:req.body.price
+	}
+	itemModel.replaceOne({"_id":req.params.id},item).then(function() {
+		return res.status(200).json({"Message":"Success"})
+	}).catch(function(err) {
+		console.log("Failed editing items, Reason",err)
+		return res.status(500).json({"Message":"Internal server error"});
+	})
+})
+
 module.exports = router;

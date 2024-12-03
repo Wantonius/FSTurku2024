d3fbc9e1e8ba48d208a37991d65b5d6b88091eda
Erno Hentonen <erno.hentonen@opiframe.com>
2024-12-03 10:57:59 +0200

    hello navigation

---
+++ react-native/05_hello_navigation/components/GreetingForm.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import {View,Pressable,Text,TextInput,StyleSheet} from 'react-native';
+
+const GreetingForm = (props) => {
+	
+}
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1,
+		justifyContent:"center",
+		alignItems:"center"
+	},
+	row:{
+		flex:1,
+		flexDirection:"row",
+		alignItems:"center",
+		justifyContent:"space-between"
+	},
+	buttonRow:{
+		justifyContent:"center"
+	},
+	button:{
+		height:80,
+		width:110,
+		backgroundColor:"green",
+		alignItems:"center",
+		justifyContent:"center"
+	},
+	textInput:{
+		backgroundColor:"lightblue",
+		width:200
+	}
+})
+
+export default GreetingForm;

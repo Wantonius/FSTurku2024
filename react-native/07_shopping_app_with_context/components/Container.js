cb5151155a21d510b2b7ab3709b5f95a5db36ef9
Erno Hentonen <erno.hentonen@opiframe.com>
2024-12-04 14:35:45 +0200

    shopping app

--- react-native/07_shopping_app_with_context/components/Container.js
+++ react-native/07_shopping_app_with_context/components/Container.js
@@ -1,9 +1,53 @@
 import LoginPage from './LoginPage';
+import ShoppingForm from './ShoppingForm';
+import ShoppingList from './ShoppingList';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import useAppState from '../hooks/useAppState';
+import useAction from '../hooks/useAction';
+import {useEffect} from 'react';
+
+const Stack = createNativeStackNavigator();
 
 const Container = (props) => {
+	
+	const {isLogged,error,loading} = useAppState();
+	const {getList} = useAction();
+	
+	useEffect(() => {
+		if(isLogged) {
+			getList();
+		}
+	},[isLogged])
+
+	let title = "Shopping App"
+	if(loading) {
+		title = "Loading..."
+	}
+	if(error) {
+		title = error;
+	}
 
 	return (
-		<LoginPage/>
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={{
+				title:title,
+				headerStyle:{
+					backgroundColor:"#00CCCC"
+				}
+			}}>
+			{isLogged ? (
+				<>
+					<Stack.Screen name="ShoppingList" component={ShoppingList}/>
+					<Stack.Screen name="Add Item" component={ShoppingForm}/>
+				</>
+			): (
+				<>
+					<Stack.Screen name="LoginPage" component={LoginPage}/>
+				</>
+			)}
+			</Stack.Navigator>
+		</NavigationContainer>
 	)
 }
 
---
+++ react-native/07_shopping_app_with_context/components/ShoppingList.js
@@ -0,0 +1,97 @@
+import {FlatList,View,Pressable,Text,StyleSheet} from 'react-native';
+import useAction from '../hooks/useAction';
+import useAppState from '../hooks/useAppState';
+
+
+const ShoppingList = (props) => {
+	const {list} = useAppState();
+	const {remove,changeMode,logout} = useAction();
+	
+	return(
+		<View style={styles.container}>
+			<View style={styles.buttonBox}>
+				<Pressable style={styles.navigateButton}
+					onPress={() => props.navigation.navigate("Add Item")}>
+					<Text style={styles.textStyle}>Add new item</Text>
+				</Pressable>
+				<Pressable style={[styles.navigateButton,styles.logoutButton]}
+					onPress={logout}>
+					<Text style={styles.textStyle}>Logout</Text>
+				</Pressable>
+			</View>
+			<View style={styles.listBox}>
+				<FlatList data={list}
+							renderItem={
+								({item}) => {
+									return(
+										<View style={styles.row}>
+											<Text style={styles.textStyle}>Type:{item.type}</Text>
+											<Text style={styles.textStyle}>Count:{item.count}</Text>
+											<Text style={styles.textStyle}>Price:{item.price}</Text>
+											<Pressable style={styles.buttonStyle}
+												onPress={() => remove(item._id)}>
+												<Text style={styles.textStyle}>Remove</Text>
+											</Pressable>
+											<Pressable style={styles.buttonStyle}
+												onPress={() => {
+													changeMode("Edit",item);
+													props.navigation.navigate("Add Item");
+												}}>
+												<Text style={styles.textStyle}>Edit</Text>
+											</Pressable>
+										</View>
+									)
+								}
+							} keyExtractor={(item) => item._id}/>
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1
+	},
+	buttonBox:{
+		flex:1,
+		flexDirection:"row",
+		justifyContent:"space-evenly",
+		alignItems:"center"
+	},
+	listBox:{
+		flex:10,
+		justifyContent:"center",
+		alignItems:"center"
+	},
+	navigateButton:{
+		flex:1,
+		alignItems:"center",
+		justifyContent:"center",
+		backgroundColor:"lightblue"
+	},
+	logoutButton:{
+		backgroundColor:"lightgreen"
+	},
+	textStyle:{
+		fontFamily:"sans-serif",
+		fontWeight:"bold",
+		fontSize:13,
+		padding:2
+	},
+	buttonStyle:{
+		padding:3,
+		width:70,
+		height:50,
+		backgroundColor:"red",
+		alignItems:"center",
+		justifyContent:"center"
+	},
+	row:{
+		flex:1,
+		flexDirection:"row",
+		justifyContent:"center",
+		alignItems:"center"
+	}
+})
+
+export default ShoppingList
